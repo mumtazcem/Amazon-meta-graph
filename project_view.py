cda435d1acdf5cb4e0f5b1ba746b751a62ae080f
msezer <msezer2009@gmail.com>
2021-01-24 01:15:32 +0300

    Random Graph Generator Added.

--- project_view.py
+++ project_view.py
@@ -1,3 +1,68 @@
+# Marcin Damek, M.Cem Eriş, Mehmet Sezer, Recep O. Yıldız
+# {damek20, erismu, sezer20, yildizr} @itu.edu.tr
+# ITU BLG 549E Graph Theory and Algorithms
+# Project
+# Reference Metadata: https://snap.stanford.edu/data/amazon-meta.html
 # Visualization of Graphs and Adjacency Matrices
+
 # TODO Merge with project.py after all done.
 
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+
+def er_random_graph_generator(n, p, ng, seed, w_base, w_top):
+    """Returns Erdos Renyi Graphs.
+
+        Parameters
+        ----------
+        n : int
+            The number of nodes.
+        p : float
+            The probability of adding a new edge for each edge.
+        ng : int
+            The number of graphs.
+        seed : int
+            The seed for the random number generator.
+        w_base : int
+            The base value for edge weight.
+        w_top : int
+            The maximum value for edge weight.
+
+        Returns
+        -------
+        Erdos Renyi Graphs.
+
+        Notes
+        -----
+        Note that the seed is increased in every step of for loop.
+        """
+
+    f_er_graph_list = []
+    for i in range(0, ng):
+        f_g = nx.erdos_renyi_graph(n, p, seed + i, directed=False)
+        for (u, v, w) in f_g.edges(data=True):
+            w['weight'] = random.randint(w_base, w_top)
+        f_er_graph_list.append(f_g)
+    return f_er_graph_list
+
+
+def test_project_view():
+    n = 1000
+    p = 0.4
+    ng = 1
+    seed = 2021
+    w_base = 0
+    w_top = 1024
+
+    er_graph_list = er_random_graph_generator(n, p, ng, seed, w_base, w_top)
+
+    plt.figure(num=None, figsize=(15, 15), dpi=100, facecolor='#FFCC66')
+    plt.axis('off')
+    nx.draw_networkx(er_graph_list[0])
+    plt.title("NetworkData")
+    plt.show()
+
+
+test_project_view()
