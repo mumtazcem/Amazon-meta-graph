4211dc7bb6608e1c7e2c0eea83d3d36fdb03c8e6
rorroror <ronuryildiz@gmail.com>
2021-01-31 18:11:38 +0300

    remove zeros

--- generate_weighted.py
+++ generate_weighted.py
@@ -8,8 +8,8 @@ start_time = time.time()
 print("Started...")
 
 # to be read from
-g1_file = "saved_dataframes/g1Db.csv"
-g2_file = "saved_dataframes/g2Db.csv"
+g1_file = "saved_dataframes/g1Db_clean.csv"
+g2_file = "saved_dataframes/g2Db_clean.csv"
 
 # to be written to
 adj1_file = "saved_adj_matrices/adj1_full.csv"
@@ -34,27 +34,28 @@ run_G2 = True
 
 if run_G1:
     print("Running G1")
+    idDict = {}
+    j = 0
+    for i in g2['Id']:
+        idDict[i] = j
+        j = j +1
     for index, row in g1.iterrows():
-        if row['totalSimilar'] > 0:
-            for i in range(5):
-                # similarity_indices: s1, s2, s3, s4 ,s5
-                similarity_index = "s" + str(i + 1)
-                similar_product_asin = row[similarity_index]
-                # if the similar cell is null
-                if isinstance(row[similarity_index], float):
-                    continue
-                similar_product_row = g1[g1['ASIN'].str.contains(similar_product_asin)]
-                found_row, column = similar_product_row.shape
-                # if product is found
-                if found_row > 0:
-                    # if no weight is assigned
-                    x = row['nodeId']
-                    y = similar_product_row['nodeId'].iat[0]
-                    if adj_1[x][y] == 0:
-                        # sum both products' totalVote values
-                        weight = row['totalVote'] + similar_product_row['totalVote'].iat[0]
-                        # assign the weight to the corresponding spot
-                        adj_1[x][y] = weight
+        for i in range(5):
+            # similarity_indices: s1, s2, s3, s4 ,s5
+            similarity_index = "s" + str(i + 1)
+            similar_product_asin = row[similarity_index]
+            # if the similar cell is null
+            if np.isnan(row[similarity_index]):
+                continue
+            # if no weight is assigned
+             x = idDict.get(row['Id'])
+            y = idDict.get(row[similarity_index])
+            if adj_1[x][y] == 0:
+                # sum both products' totalVote values
+                weight = row['totalVote'] + row[vote_index]
+                # assign the weight to the corresponding spot
+                adj_1[x][y] = weight
+
     G1 = nx.from_numpy_matrix(adj_1)
     num_of_nodes, num_of_edges = pv.get_nodes_and_edges_number(G1)
     print("Number of nodes :", num_of_nodes, "number of edges :", num_of_edges)
@@ -70,27 +71,28 @@ print("G1 is finished in --- %s seconds ---" % (g1_time - start_time))
 
 if run_G2:
     print("Running G2")
+    idDict = {}
+    j = 0
+    for i in g2['Id']:
+        idDict[i] = j
+        j = j +1
     for index, row in g2.iterrows():
-        if row['totalSimilar'] > 0:
-            for i in range(5):
-                # similarity_indices: s1, s2, s3, s4 ,s5
-                similarity_index = "s" + str(i + 1)
-                similar_product_asin = row[similarity_index]
-                # if the similar cell is null
-                if isinstance(row[similarity_index], float):
-                    continue
-                similar_product_row = g2[g2['ASIN'].str.contains(similar_product_asin)]
-                found_row, column = similar_product_row.shape
-                # if product is found
-                if found_row > 0:
-                    # if no weight is assigned
-                    x = row['nodeId']
-                    y = similar_product_row['nodeId'].iat[0]
-                    if adj_2[x][y] == 0:
-                        # sum both products' totalVote values
-                        weight = row['totalVote'] + similar_product_row['totalVote'].iat[0]
-                        # assign the weight to the corresponding spot
-                        adj_2[x][y] = weight
+       for i in range(5):
+            # similarity_indices: s1, s2, s3, s4 ,s5
+            similarity_index = "s" + str(i + 1)
+            vote_index = "v" + str(i + 1)
+            similar_product_asin = row[similarity_index]
+            # if the similar cell is null
+            if np.isnan(row[similarity_index]):
+                continue
+            x = idDict.get(row['Id'])
+            y = idDict.get(row[similarity_index])
+            if adj_2[x][y] == 0:
+                # sum both products' totalVote values
+                weight = row['totalVote'] + row[vote_index]
+                # assign the weight to the corresponding spot
+                adj_2[x][y] = weight
+                #print(weight)
 
     G2 = nx.from_numpy_matrix(adj_2)
     num_of_nodes, num_of_edges = pv.get_nodes_and_edges_number(G2)
@@ -99,10 +101,11 @@ if run_G2:
     # pv.show_adjacency_matrix(adj_2)
     # pv.show_adjacency_matrix_from_graph(G2)
     # pv.draw_networkx_graph(G2)
-    pv.plot_degree_dist(G2)
 
 g2_time = time.time()
 print("G2 is finished --- %s seconds ---" % (g2_time - g1_time))
+
+pv.plot_degree_dist(G2)
 
 
 print("All finished in %s seconds." % (time.time() - start_time))
